2f7d8ef00186d6540d204478c5d763b5ee0316f0
Ryan Driscoll <driscollrp@gmail.com>
2017-05-17 12:24:24 -0500

    signature component integrated, required adding React to RCT stuff in react-native-sketch

--- index.ios.js
+++ index.ios.js
@@ -11,17 +11,13 @@ import {
   Text,
   View
 } from 'react-native';
+import Signature from './app/components/Signature';
 
 export default class MobileSketchCard extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
+        <Signature />
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
